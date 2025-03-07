053de021675327b04fa56c08f515414a560c63b7
NatalyaAtyukova <atukowa.natalya@yandex.ru>
2025-03-08 00:17:01 +0200

    Update VesionCode for RuStore realise

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.tarotapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 6
+        versionName = "6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
